de70d0c1f4bf64a393894bb615688d3672f9a076
gengjian1203 <gengjian1203@foxmail.com>
2021-06-12 23:27:17 +0800

    feat:(project)新增分享交互

--- assets/pages/Login/Login.ts
+++ assets/pages/Login/Login.ts
@@ -11,6 +11,7 @@ export class Login extends Component {
   // serializableDummy = 0;
   start() {
     console.log("Login start...");
+    this.passiveShare();
     // [3]
   }
 
@@ -21,6 +22,36 @@ export class Login extends Component {
   // 点击测试功能
   handleBtnTestClick() {
     console.log("handleBtnTestClick");
+  }
+
+  // 监听被动分享
+  passiveShare() {
+    // 监听小程序右上角菜单的「转发」按钮
+    // @ts-ignore
+    if (typeof wx === "undefined") {
+      return;
+    }
+    // 显示当前页面的转发按钮
+    // @ts-ignore
+    wx.showShareMenu({
+      success: (res: any) => {
+        console.log("开启被动转发成功！");
+      },
+      fail: (err: any) => {
+        console.log("开启被动转发失败！", err);
+      },
+    });
+    // 获取当前转发人的信息，
+    // @ts-ignore
+    wx.onShareAppMessage(() => {
+      return {
+        title: "雪人坛说，等你来玩~",
+        imageUrlId: "8io3vSE1RFKdT3hm9bNIOA==",
+        imageUrl:
+          "https://mmocgame.qpic.cn/wechatgame/2gGBq1F0NK3s6JyIkVPYxknqibqcZql0IOX2iag0c7477lialpFGf5ru3EMapicJNiaRx/0", // 分享图片要放在 wechatgame/res/raw-assets 路径下
+        query: `shareId=`,
+      };
+    });
   }
 }
 
